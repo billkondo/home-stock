5e17f108c9907ec0064e784f2f490274ff9eacc5
billkondo <willkondo@gmail.com>
2019-07-27 21:47:25 -0300

    Adding font changes in management section selection

--- src/pages/management.tsx
+++ src/pages/management.tsx
@@ -1,13 +1,15 @@
 import React, { useReducer } from 'react';
 import { Grid, Tabs, Tab } from '@material-ui/core';
-import { withRouter, RouteComponentProps, Link } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import Products from '../components/management/products/Products';
 import Tags from '../components/management/tags/Tags';
 import { ManagementTabType, routes } from '../config/routes';
 
+type TabValues = 0 | 1;
+
 type State = {
-  tab: number;
+  tab: TabValues;
 };
 
 type MatchParamsType = {
@@ -16,7 +18,7 @@ type MatchParamsType = {
 
 type Props = RouteComponentProps<MatchParamsType>;
 
-type Action = { type: 'CHANGE_TAB'; tab: number };
+type Action = { type: 'CHANGE_TAB'; tab: TabValues };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
@@ -42,7 +44,7 @@ const Management: React.FC<Props> = ({ match, history }) => {
     init
   );
 
-  const handleChange = (e: React.ChangeEvent<{}>, newValue: number) => {
+  const handleChange = (e: React.ChangeEvent<{}>, newValue: TabValues) => {
     history.push(routes.management(newValue === 0 ? 'products' : 'tags'));
 
     dispatch({
@@ -51,15 +53,24 @@ const Management: React.FC<Props> = ({ match, history }) => {
     });
   };
 
+  const getFontFamily = (tab: TabValues, selectedTab: TabValues) =>
+    tab === selectedTab ? 'Fredoka One' : '';
+
   return (
     <Grid container justify="center">
       <Grid item md={8} xs={10}>
         <Grid container direction="column">
           <Grid item>
             <Tabs value={state.tab} onChange={handleChange}>
-              <Tab label="Produtos" />
+              <Tab
+                label="Produtos"
+                style={{ fontFamily: getFontFamily(0, state.tab) }}
+              />
 
-              <Tab label="Categorias" />
+              <Tab
+                label="Categorias"
+                style={{ fontFamily: getFontFamily(1, state.tab) }}
+              />
             </Tabs>
           </Grid>
 
